8df5106d314f9e4e0cebad735d22dbb27b8b6f3d
Romlaibn <romainpaganotto47@gmail.com>
2020-06-21 17:08:09 +0200

    fazfeeza

--- install/install_bind.sh
+++ install/install_bind.sh
@@ -3,6 +3,6 @@
 rm /var/docker/controlesr/bind/bind/etc/named.conf.local
 
 mv /var/docker/controlesr/install/bind/named.conf.local /var/docker/controlesr/bind/bind/etc/named.conf.local
-mv /var/docker/controlesr/install/bind/heberg.projet.db /var/docker/controlesr/bind/bind/etc/named.conf.local
+mv /var/docker/controlesr/install/bind/heberg.projet.db /var/docker/controlesr/bind/bind/etc/heberg.projet.db
 
 docker restart controlesr_bind9_1
