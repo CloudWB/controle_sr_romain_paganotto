e5006c384847971da0ef96d1fb30668197089ac0
Romlaibn <romainpaganotto47@gmail.com>
2020-06-21 20:48:36 +0200

    fzaefzeafaz

--- install/install_user.sh
+++ install/install_user.sh
@@ -19,14 +19,22 @@ done
 #on commence à passer à des vérifications
 #on évite que le login soit identique au mdp
 if [ "$login" = "$password" ] ; then
-	echo "[!] Password same as login"
+	echo "[ERREUR] Votre mot de passe est identique à votre mot de passe."
 	exit 1
 
 #on verifie que le mot de passe soit nul
 elif [ -z "$password" ] ; then
-	echo "[!] Password is null"
+	echo "[ERREUR] Votre mot de passe est nul."
 	exit 1
 fi
+
+#on vérifie que maintenant le nom du sous domaine n'existe pas
+for FILE in /var/docker/controlesr/vhosts/heberg/subdomains/* ; do
+	if [ "/var/docker/controlesr/vhosts/heberg/subdomains/$sousDomaine" = "$FILE" ] ; then
+		echo "[ERREUR] Le nom du sous domaine existe déjà."
+		exit 1
+	fi
+done
 
 #on hach le mot de passe
 hash=$(mkpasswd -m sha-512 "$password")
@@ -36,4 +44,22 @@ useradd -m -p "$hash" -s /bin/bash "$login"
 
 echo "[SUCCES] L'utilisateur a été ajouté"
 
+mkdir /var/docker/controlesr/vhosts/heberg/subdomains/$sousDomaine
 
+touch /var/docker/controlesr/install/docker-compose.yml
+
+echo "version: '3.1'" >> /var/docker/controlesr/install/docker-compose.yml
+echo "services:" >> /var/docker/controlesr/install/docker-compose.yml
+echo "  db:" >> /var/docker/controlesr/install/docker-compose.yml
+echo "    image: mariadb" >> /var/docker/controlesr/install/docker-compose.yml
+echo "    restart: always" >> /var/docker/controlesr/install/docker-compose.yml
+echo "    environment:" >> /var/docker/controlesr/install/docker-compose.yml
+echo "      DB_USER: $login" >> /var/docker/controlesr/install/docker-compose.yml
+echo "      DB_PASSWORD: $password" >> /var/docker/controlesr/install/docker-compose.yml
+echo "      DB_NAME: $login" >> /var/docker/controlesr/install/docker-compose.yml
+echo "    volumes:" >> /var/docker/controlesr/install/docker-compose.yml
+echo "      - ./var/docker/controlesr/vhosts/heberg/subdomains/$sousDomaine/db:/var/lib/mysql" >> /var/docker/controlesr/install/docker-compose.yml
+
+docker-compose up -d
+
+rm /var/docker/controlesr/install/docker-compose.yml
