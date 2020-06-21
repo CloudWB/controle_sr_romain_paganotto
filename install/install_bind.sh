2f270444b4830b9a7a7a36d48c92f545f82eceaf
Romlaibn <romainpaganotto47@gmail.com>
2020-06-21 17:13:12 +0200

    fzeafzeaf

--- install/install_bind.sh
+++ install/install_bind.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#on supprime le fichier named.conf.local
 rm /var/docker/controlesr/bind/bind/etc/named.conf.local
+echo "Vous avez bien supprimer le dossier named.conf.local"
 
+#on deplace le fichier named.conf.local et heberg.projet.db dans notre container
 mv /var/docker/controlesr/install/bind/named.conf.local /var/docker/controlesr/bind/bind/etc/named.conf.local
 mv /var/docker/controlesr/install/bind/heberg.projet.db /var/docker/controlesr/bind/bind/etc/heberg.projet.db
+echo "Vous avez bien déplacer les fichiers : named.conf.local, heberg.projet.db"
 
+#on restart notre container controlesr_bind9_1 pour actualiser notre nouveau dns heberg.projet
 docker restart controlesr_bind9_1
+echo "Restart effectué, votre dns est activé. Veuillez vérifier avec la commande : dig @localhost heberg.projet"
